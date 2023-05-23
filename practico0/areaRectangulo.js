dd34f5b11b6bbd2968d9332d88e39831a25a0886
eperonja2023 <eperonja@yahoo.com.ar>
2023-05-23 00:18:34 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,9 @@
+var base = 5;
+var altura = 3;
+var areaRectangulo = base * altura;
+console.log('El área de un rectángulo de base ' + base + 'cm y altura ' + altura + 'cm es ' + areaRectangulo + 'cm2')
+
+var base = 12.5;
+var altura = 5.3;
+var areaRectangulo = base * altura;
+console.log('El área de un rectángulo de base ' + base + 'cm y altura ' + altura + 'cm es ' + areaRectangulo + 'cm2')
---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+var nro1 = 5;
+var nro2 = 7;
+var nro3 = 9;
+var promedio3 = ((nro1+nro2+nro3)/3);
+console.log('El promedio entre ' + nro1 + ', ' + nro2 + ' y ' + nro3 + ' es: ' + promedio3)
+
+var nro1 = -2;
+var nro2 = -3;
+var nro3 = -8;
+var promedio3 = ((nro1+nro2+nro3)/3);
+console.log('El promedio entre ' + nro1 + ', ' + nro2 + ' y ' + nro3 + ' es: ' + promedio3)
+
+var nro1 = -1.2;
+var nro2 = 5.3;
+var nro3 = -3.3;
+var promedio3 = ((nro1+nro2+nro3)/3);
+console.log('El promedio entre ' + nro1 + ', ' + nro2 + ' y ' + nro3 + ' es: ' + promedio3)
