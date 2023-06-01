130d4f96dab7f06ef24e0b93b35aa080984f2b05
eperonja2023 <eperonja@yahoo.com.ar>
2023-06-01 16:47:19 -0300

    borré todo para volver a subir todo

--- practico0/conversionGrados.js
+++
@@ -1,7 +0,0 @@
-var gradosF = 50;
-var gradosC = (gradosF - 32)*5/9;
-console.log(gradosF + ' grados Fahrenheit equivalen a ' + gradosC + ' centigrados')
-
-var gradosF = 75;
-var gradosC = (gradosF - 32)*5/9;
-console.log(gradosF + ' grados Fahrenheit equivalen a ' + gradosC + ' centigrados')
