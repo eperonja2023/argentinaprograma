0175fa34b5770c0f0f9296827f15399585174274
eperonja2023 <eperonja@yahoo.com.ar>
2023-07-06 13:45:46 -0300

    Subo practico4-extras 'terminado'

--- practico4/piedraPapelTijerasConExtras.js
+++ practico4/piedraPapelTijerasConExtras.js
@@ -1,147 +1,99 @@
-console.log('');
+console.clear();
 
 const PIEDRA = 'piedra';
 const PAPEL = 'papel';
 const TIJERAS = 'tijeras';
+
 
 function elegirModo() {
     const readlineSync = require('readline-sync');
     let modoJuego = readlineSync.question('*** Bienvenido a PIEDRA-PAPEL-TIJERAS !!! *** \n' +
         '\n' +
         'Elegi el modo de juego.... \n' +
-        '(1) Una ronda \n' +
-        '(2) Dos rondas \n' +
-        '(3) El mejor de tres \n' +
+        ' (1) Una ronda \n' +
+        ' (2) Dos rondas \n' +
+        ' (3) El mejor de tres \n' +
         '\n' +
         'Tu eleccion =====> '
     );
+    console.log('');
     modoJuego = Number(modoJuego);
 
     while (![1, 2, 3].includes(modoJuego)) {    //Alternativa: (jugadaUsuario !== 1 && jugadaUsuario !== 2 && jugadaUsuario !== 3) {
         const readlineSync = require('readline-sync');
-        modoJuego = readlineSync.question('*VALOR NO VALIDO !!!  PARA JUGAR ELEGI "1", "2" o "3"');
+        modoJuego = readlineSync.question('*VALOR NO VALIDO !!!  PARA JUGAR ELEGI "1", "2" o "3": ');
         modoJuego = Number(modoJuego);
     }
-    return modoJuego;
+   return modoJuego;
 }
 
-// switch (elegirModo()) {
-//     case 1:
-//         ronda();
-//         console.log('FIN DEL JUEGO');
-//         console.log('');
-//         break;
-//     case 2:
-//         for (let i = 0; i < elegirModo(); i++) {
-//             ronda();
-            
-//         }
-//         console.log('')
-//         break;
-// }
 
 let cantidadRondas = elegirModo();
 //console.log(cantidadRondas);
-
-for (let i = 1; i < cantidadRondas+1; i++) {
-    console.log('Ronda ' + (i) + ':');
-    console.log(prueba());
-    //return (ronda());
+for (let i = 1; i < cantidadRondas + 1; i++) {
+    if (cantidadRondas > 1) {
+        console.log('Ronda ' + (i) + ':');
+    }
+        console.log(ronda());
+    if (i == cantidadRondas) {
+        console.log('');
+        console.log('FIN DEL JUEGO');
+        console.log('');
+    }
 }
 
 
 
 function ronda() {
-function obtenerJugadaComputadora() {
-    //let valores = {0:'piedra', 1:'papel', 2:'tijeras'};
-    let valores = {0:PIEDRA, 1:PAPEL, 2:TIJERAS};
-    let jugadaComputador = valores[Math.floor(Math.random() * 3)];
-    return jugadaComputador;
-}
+    function obtenerJugadaComputadora() {
+        //Ejercicio original: let valores = {0:'piedra', 1:'papel', 2:'tijeras'};
+        let valores = { 0: PIEDRA, 1: PAPEL, 2: TIJERAS };
+        let jugadaComputador = valores[Math.floor(Math.random() * 3)];
+        return jugadaComputador;
+    }
 
-//console.log(obtenerJugadaComputadora())
+        //console.log(obtenerJugadaComputadora())
 
 
-function obtenerJugadaUsuario() {
-    const readlineSync = require('readline-sync');
-    let jugadaUsuario = readlineSync.question(" ====> INGRESA UNA OPCION PARA EMPEZAR A JUGAR...: ");
-    jugadaUsuario = jugadaUsuario.toLowerCase();
-
-    while (jugadaUsuario !== PIEDRA && jugadaUsuario !== PAPEL && jugadaUsuario !== TIJERAS) {
+    function obtenerJugadaUsuario() {
         const readlineSync = require('readline-sync');
-        jugadaUsuario = readlineSync.question('VALOR NO VALIDO !!!  PARA JUGAR ELEGI ENTRE "piedra", "papel" o "tijeras" E INGRESALO: ');
+        let jugadaUsuario = readlineSync.question(' ====> INGRESA UNA OPCION PARA EMPEZAR A JUGAR...: ');
         jugadaUsuario = jugadaUsuario.toLowerCase();
+
+        while (jugadaUsuario !== PIEDRA && jugadaUsuario !== PAPEL && jugadaUsuario !== TIJERAS) {
+            const readlineSync = require('readline-sync');
+            jugadaUsuario = readlineSync.question('VALOR NO VALIDO !!!  PARA JUGAR ELEGI ENTRE "piedra", "papel" o "tijeras" E INGRESALO: ');
+            jugadaUsuario = jugadaUsuario.toLowerCase();
+        }
+        return jugadaUsuario;
     }
-    return jugadaUsuario;
-}
-//console.log(obtenerJugadaUsuario());
+        //console.log(obtenerJugadaUsuario());
 
-let empates = 0;
-let ganaCompu = 0;
-let ganaUsuario = 0;
 
-function determinarGanador(pc, jugador) {
-   let resultado = '';
-   if (pc === jugador) {
-       resultado = 'Empate';
-       empates = empates + 1;
-    } else if (pc == PIEDRA && jugador == TIJERAS || pc == PAPEL && jugador == PIEDRA || pc == TIJERAS && jugador == PAPEL) {
-        resultado = 'Gana la computadora';
-        ganaCompu = ganaCompu + 1;
-    } else {
-        resultado = 'Gana el usuario';
-        ganaUsuario = ganaUsuario + 1;
+
+    function determinarGanador(pc, jugador) {
+        let resultado = '';
+        if (pc === jugador) {
+            resultado = 'Empate !!!';
+        } else if (pc == PIEDRA && jugador == TIJERAS || pc == PAPEL && jugador == PIEDRA || pc == TIJERAS && jugador == PAPEL) {
+            resultado = 'Gana la computadora !!!';
+        } else {
+            resultado = 'Gana el usuario !!!';
+        }
+        return resultado;
     }
-    return resultado;
-}
-//console.log(determinarGanador(obtenerJugadaComputadora(), obtenerJugadaUsuario()));
-
-let jugadaComputador = obtenerJugadaComputadora();
-let jugadaUsuario = obtenerJugadaUsuario();
-let ganador = determinarGanador(jugadaComputador, jugadaUsuario);
-
-console.log('La computadora eligio: %s', jugadaComputador);
-console.log('El usuario eligio: %s', jugadaUsuario);
-console.log('El resultado fue: %s', ganador);
-console.log('');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let jugadaCompu = obtenerJugadaComputadora();
-let jugadaJugador = obtenerJugadaUsuario();
-let ganador = determinarGanador();
-
-function prueba() {
-    console.log(jugadaCompu);
-    console.log(jugadaJugador);
-    console.log(ganador);
+        //console.log(determinarGanador(obtenerJugadaComputadora(), obtenerJugadaUsuario()));
 
     let jugadaComputador = obtenerJugadaComputadora();
     let jugadaUsuario = obtenerJugadaUsuario();
     let ganador = determinarGanador(jugadaComputador, jugadaUsuario);
 
-    console.log('La computadora eligio: %s', jugadaComputador);
-    console.log('El usuario eligio: %s', jugadaUsuario);
-    console.log('El resultado fue: %s', ganador);
-    console.log('');
 
+    let cartelResultado = `\n` + 
+    `La computadora eligio: ${jugadaComputador} \n` +
+    `El usuario eligio: ${jugadaUsuario} \n` + 
+    `El resultado fue: ${ganador} \n` +
+    ``;
+
+    return cartelResultado;
 }
